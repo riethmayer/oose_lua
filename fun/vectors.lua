fd139c006ea06900d977308c2d9742b9326fe263
Jan Riethmayer <jan@riethmayer.de>
2010-04-30 18:00:00 +0200

    refactor metatable definition; added vector_multiplication.

--- fun/vectors.lua
+++ fun/vectors.lua
@@ -1,27 +1,34 @@
 Vector = {}
+function _vector_addition(v1,v2)
+   if compatible_vectors(v1,v2) then
+      local result = Vector:new()
+      for i,v in ipairs(v1) do
+         result[i] = v1[i] + v2[i]
+      end
+      return result
+   else
+      return nil
+   end
+end
+
+function _vector_multiplication(v1,v2)
+   if compatible_vectors(v1,v2) then
+      local result = Vector:new()
+      for i,v in ipairs(v1) do
+         result[i] = normalize(v1[i], v2[i])
+      end
+      return result
+   else
+      return nil
+   end
+end
+
 _VectorBehavior = {
-   __add = function(v1,v2)
-              --valid_for_add(v1,v2)
-              local result = Vector:new()
-              for i,v in ipairs(v1) do
-                 result[i] = v1[i] + v2[i]
-              end
-              return result
-           end
+   -- component wise addition
+   __add = function(v1,v2) return _vector_addition(v1,v2)  end,
+   -- component wise normalization
+   __mul = function(v1,v2) return _vector_multiplication(v1,v2) end
 }
-
-function valid_for_add(v1,v2)
-   result = is_vector(v1) and is_vector(v2) and (#v1 == #v2)
-   return result
-end
-
-function is_vector(v)
-   return v._class == Vector
-end
-
-function unless(val)
-   return (val ~= true)
-end
 
 function Vector:new(...)
    local o = setmetatable({}, _VectorBehavior)
@@ -30,4 +37,21 @@ function Vector:new(...)
    end
    o._class = self
    return o
+end
+
+function compatible_vectors(v1,v2)
+   result = is_vector(v1) and is_vector(v2) and (#v1 == #v2)
+   return result
+end
+
+function is_vector(v)
+   return v._class == Vector
+end
+
+function normalize(x,y)
+   if(type(x) == "number" and type(y) == "number") then
+      return math.sqrt(x^2 + y^2)
+   else
+      return nil
+   end
 end
--- fun/vectors_spec.lua
+++ fun/vectors_spec.lua
@@ -3,56 +3,90 @@ require('vectors')
 
 LSpec:setup()
 
+-- describing basic type
 it("should be a Vector", 
    function()
       v= Vector:new(1,2)
       return (v._class == Vector)
    end)
-
+it("should not be classified as vector",
+   function()
+      return is_vector({}) == false
+   end)
+it("should be classified as vector without components",
+   function()
+      return is_vector(Vector:new())
+   end)
+it("should be classified as vector with components",
+   function()
+      return is_vector(Vector:new(1,2))
+   end)
 it("should assign components",
    function()
       v1 = Vector:new(1,2)
       return v1[1] == 1 and v1[2] == 2
    end)
 
+-- describe add
 it("should add components",
    function()
       v1 = Vector:new(2,5)
       result = v1 + v1
       return (4 == result[1]) and (10 == result[2])
    end)
+it("should not add vectors with different length",
+   function()
+      v1 = Vector:new(3,4)
+      v2 = Vector:new(4)
+      result = v1 + v2
+      return result == nil
+   end)
 
+-- describe validation for vector operations
 it("should not validate invalid tables for addition",
    function()
       v1 = Vector:new(2,5)
       v2 = {}
-      result = valid_for_add(v1,v2)
+      result = compatible_vectors(v1,v2)
       return result == false
    end)
-
 it("should not validate vectors with different length",
    function()
       v1 = Vector:new(1,2,3)
       v2 = Vector:new(1,2)
-      result = valid_for_add(v1,v2)
+      result = compatible_vectors(v1,v2)
       return result == false
    end)
-
-it("should not be classified as vector",
+it("should normalize values",
    function()
-      return is_vector({}) == false
+      return normalize(3,4) == 5
+   end)
+it("should return nil for nil component",
+   function()
+      return normalize(3,nil) == nil and normalize(nil,1) == nil
    end)
 
-it("should be classified as vector without components",
+-- vector multiplication
+it("should multiply simple vectors",
    function()
-      return is_vector(Vector:new())
+      v1 = Vector:new(3)
+      v2 = Vector:new(4)
+      result = v1 * v2
+      return result[1] == 5
    end)
-
-it("should be classified as vector with components",
+it("should multiply more elaborate vectors",
    function()
-      return is_vector(Vector:new(1,2))
+      v1 = Vector:new(3,4)
+      v2 = Vector:new(4,3)
+      result = v1 * v2
+      return result[1] == 5 and result[2] == 5
    end)
-
-
+it("should not multiply vectors with different length",
+   function()
+      v1 = Vector:new(3,4)
+      v2 = Vector:new(4)
+      result = v1 * v2
+      return result == nil
+   end)
 
 LSpec:teardown()

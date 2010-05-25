61ab4e0c49a6456e2db484f166ccf17904fe9367
Jan Riethmayer <jan@riethmayer.de>
2010-05-25 15:12:28 +0200

    Object creation.

--- aufgaben/aufgabe2/object.lua
+++ aufgaben/aufgabe2/object.lua
@@ -6,27 +6,35 @@
 --   o:classname()     ;; this is an instance method
 Object = {
    _super = nil,
-   classname  = "Object",
-   _class_methods = {}
+   _class_methods = {
+      classname  = "Object"
+   }
 }
+-- delegate to its class class methods
+local class_method_lookup = {
+   __index   = Object._class_methods }
+setmetatable(Object,class_method_lookup)
 -- self is a class name for a given Class, which is to be instantiated
 -- provide
 function Object:new()
-   assert( nil ~= self)
    local object = {
       _class = self,
       _instance_methods =  {
+         -- o:classname() is a call, and _class.classname is string value.
+         -- that's why you can't simply delegate that one to Object.
          classname = function(instance)
                         return(instance._class.classname)
-                     end   }}
+                     end  }
+   }
    -- delegate to instance methods first
    local instance_method_lookup   = {
       __index   = object._instance_methods }
    setmetatable(object,instance_method_lookup)
-   -- delegate to its class's class methods
+
+   -- after method isn't found in instance delegate to class methods
    local class_method_lookup = {
-      __index   = object._class._class_methods }   
+      __index   = object._class._class_methods }
    setmetatable(object._instance_methods,class_method_lookup)
-   
+
    return object
 end
--- aufgaben/aufgabe2/object_spec.lua
+++ aufgaben/aufgabe2/object_spec.lua
@@ -7,12 +7,11 @@ it("should create a new object",
       o = Object:new()
       return (nil ~= o)
    end)
-
 ----------------------------------------------------------------------------------
-it("should should have a classname called 'Object'",
+it("should have instances with a classname called 'Object'",
    function()
       o = Object:new()      
-      return o.classname == "Object"
+      return o:classname() == "Object"
    end)
 ----------------------------------------------------------------------------------
 it("should have no superclass",
@@ -20,4 +19,20 @@ it("should have no superclass",
       o = Object:new()
       return o._super == nil
    end)
+----------------------------------------------------------------------------------
+it("should have class methods, because it is a class",
+   function()
+      return (nil ~= Object._class_methods)
+   end)
+----------------------------------------------------------------------------------
+it("should have no instance methods as a pure class",
+   function()
+      return (nil == Object._instance_methods)
+   end)
+----------------------------------------------------------------------------------
+it("should delegate method calls to _class_methods",
+   function()
+      return (Object.classname == "Object")
+   end)
+
 LSpec:teardown()
